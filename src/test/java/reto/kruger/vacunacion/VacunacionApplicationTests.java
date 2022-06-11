7909e8771202229eed56ffd7a35ac94b462718b7
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2022-06-11 01:22:49 -0500

    Initilization of the vaccination project from Kruger with spring boot

---
+++ src/test/java/reto/kruger/vacunacion/VacunacionApplicationTests.java
@@ -0,0 +1,13 @@
+package reto.kruger.vacunacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacunacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
